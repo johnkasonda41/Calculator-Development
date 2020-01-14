fcedf067de3ff234d48c75741c35c08bf05df49f
kelvin.luvila <kelvin.luvila@szi.gov.zm>
2020-01-14 10:32:23 +0200

    Feature: Added Division and DivisionTests

---
+++ Division/Division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Division
+{
+    public class Division
+    {
+
+        public static double Divide(int a, int b)
+        {
+            double c = a / b;
+            return c;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            double c = a / b;
+            return c;
+        }
+    }
+}
