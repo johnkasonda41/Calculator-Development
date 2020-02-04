f71f72de7f3a1397ba30411d0cd051b1ef2cfd5c
kelvin.luvila <kelvin.luvila@szi.gov.zm>
2020-02-04 12:45:19 +0200

    Feature: Added Interface Multiplication

---
+++ IMultiply/IMultiply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Multiplication;
+
+namespace IMultiply
+{
+    public interface IMultiply
+    {
+        public dynamic Multiply(dynamic a, dynamic b);
+    }
+    public class Multiple : IMultiply
+    {
+        public dynamic result;
+
+        public dynamic Multiply(dynamic a, dynamic b)
+        {
+            result = Multiplication.Multiplication.Multiply(a, b);
+            return result;
+        }
+
+    }
+}
