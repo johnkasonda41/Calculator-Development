aa7e7e41d862cb1cfcf1929364ff50eeb00e6fb4
johnkasonda <john.kasonda@gmail.com>
2020-02-04 11:44:12 +0200

    Fix Squareroot

--- SquarerootTests/SquarerootTests.cs
+++ SquarerootTests/SquarerootTests.cs
@@ -3,7 +3,6 @@ using Squareroot;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Helpers;
 
 namespace Squareroot.Tests
 {
@@ -22,7 +21,7 @@ namespace Squareroot.Tests
         [TestMethod()]
         public void SquarerootTest1()
         {
-            Assert.AreEqual(3.29, Helpers.Rounding.RoundTwoDecimalPlaces(Squareroot.Squarerooting(b)));
+            Assert.AreEqual(3.29, Squareroot.Squarerooting(b));
         }
     }
 }
